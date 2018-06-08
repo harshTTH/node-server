fa61514ca71ae6db6554065adc211fb015c8de2d
harshTTH <harshrai456@outlook.com>
2018-06-09 01:34:24 +0530

    change js files with heroku compatibility

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.get('/',(req,res)=>res.send('Hello server'));
 
-app.listen(3000);
+
+app.listen(PORT,()=>console.log(`Server listening at ${PORT}`));
